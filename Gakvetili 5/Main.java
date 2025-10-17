8b7d39a77ada807b54470b990a040f51b0facbda
DUT1K1 <david.maisuradze0920@gmail.com>
2025-10-17 21:22:12 +0400

    add lesson 5 materials

---
+++ Gakvetili 5/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		// System.out.println("Davit Maisuradze");
+		// System.out.println(args[0]);
+
+		System.out.println("მე მქვია " + 	args[0]);
+		System.out.println("ჩემი გვარია " + args[1]);
+		System.out.println("მე ვარ " + args[2] + " წლის!");
+	}
+}
