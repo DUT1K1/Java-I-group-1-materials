22d5fcd8edd919e7af344c4b79ceb7276023aad9
DUT1K1 <david.maisuradze0920@gmail.com>
2025-10-20 20:57:37 +0400

    add lesson 6 materials

---
+++ Gakvetili 5/Lesson 6/src/Main.java
@@ -0,0 +1,51 @@
+public class Main {
+    public static void main(String[] args) {
+//        var x = 10;
+//        x = "asdfsa";
+//
+//        char a = 'ა';
+//        System.out.println(a);
+//
+//        int gamar joba = 10
+//
+//        String data = "data";
+//        String Data = "Data";
+//        String DatA = "Data";
+//        String DaTa = "Data";
+//
+//        int ageInYears = 44;
+//        String myName = "Davit";
+//
+//        double multiDigitNumberThatIsCool = 3.14;
+//
+//        short დათა = 15;
+//        System.out.println(დათა);
+//
+//        int num;
+//        num = 21;
+
+
+        // მონაცემთა ტიპების გარდაქმნა
+//        int a = 10;
+//        long b = a;
+//        short c = a;
+//
+//        double d = 3.14;
+//        a = d;
+//        d = a;
+//
+//        int x = 15, y = 15;
+//        System.out.println("x = " + x);
+//        System.out.println("y = " + y);
+
+
+        // გადაკასტვა (type casting in java)
+        double pi = 3.94;
+        int intPi = (int) pi;
+        System.out.println("intPi = " + intPi);
+
+        int x = 129;
+        byte s = (byte) x;
+        System.out.println("byte s = " + s);
+    }
+}
